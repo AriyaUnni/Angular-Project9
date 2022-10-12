f617427b142be617e32787072a10844a5f54ad68
AriyaUnni <ariyaunni2000@gmail.com>
2022-10-12 23:36:11 +0530

    routing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CarComponent } from './car/car.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes:Routes =[
+  {
+    path:"",component:CarComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +21,8 @@ import { CarComponent } from './car/car.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
